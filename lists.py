3b4ccafd518dc7872723aadb663fc87cd53a4dc3
altlambo <95392188+altlambo@users.noreply.github.com>
2021-12-09 19:26:05 -0500

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,16 @@
+shiba = ["https://c.tenor.com/NfcO8yWXWToAAAAi/shiba-shiba-inu.gif", "https://c.tenor.com/wonYsJXbYO0AAAAC/doge-intensifies-dog-intensifies.gif", "https://c.tenor.com/MRYEizbPivUAAAAd/shib-shiba.gif", "https://c.tenor.com/puoNAOOjaUcAAAAd/shibe-snow-shibe-shaking.gif", "https://c.tenor.com/uSNaQY2nybgAAAAM/shiba-shiny-shiba.gif"]
+
+dog = ["https://bit.ly/3omsfLS", "https://bit.ly/3lyamId", "https://bit.ly/3EpMOwf", "https://bit.ly/3xZlB19", "https://bit.ly/3Gcs7Ez", "https://i.imgur.com/LRoLTlK.jpeg", "https://i.imgur.com/gtWsPu9.jpeg"]
+
+cat = ["https://c.tenor.com/rG7jWvVqOoIAAAAj/dance-cat.gif", "https://c.tenor.com/GTcT7HODLRgAAAAM/smiling-cat-creepy-cat.gif", "https://c.tenor.com/M9n4lw0XrGkAAAAM/killer-upset.gif", "https://c.tenor.com/4VY0Ykn4lN4AAAAM/cat-broken-cat.gif", "https://c.tenor.com/oTeBa4EVepMAAAAM/business-cat-working.gif", "https://c.tenor.com/Lg21skpXtU4AAAAM/cat-meme.gif", "https://c.tenor.com/ixR2FQAbsH0AAAAM/mad-cat-filter.gif", "https://c.tenor.com/2immeiuqWJkAAAAM/cat-sit.gif", "https://c.tenor.com/O-h718UvzWIAAAAM/hiding-introvert.gif", "https://c.tenor.com/uB6486L-xFMAAAAM/cat-driving-serious.gif", "https://i.imgur.com/Vo3xOZT.mp4", "https://i.imgur.com/AD3MbBi.jpeg", "https://i.imgur.com/7nLm7si.mp4", "https://i.imgur.com/xjaMIKB.png", "https://i.imgur.com/KWvtdg0.mp4"]
+
+xmas = ["https://c.tenor.com/QlbeniaH8uEAAAAM/puppy-christmas-puppy.gif", "https://c.tenor.com/6gBidH13RrMAAAAM/sweater-weather-snow.gif", "https://c.tenor.com/JqtKFAzQqoUAAAAM/shibe-snow.gif"]
+
+cope = ["https://media1.giphy.com/media/9tanal5QOsU3GiX2i5/200w.webp?cid=ecf05e47k27ih895v2utpl1bwa43wh2ynp5x8xkzohhe5wr5&rid=200w.webp&ct=g", "https://c.tenor.com/XzlAiy78P-EAAAAj/cry-about-it-cope.gif", "https://c.tenor.com/0PGO_abHKnYAAAAM/cope-seethe.gif", "https://c.tenor.com/mJ2S54Lu_l8AAAAM/cope-cope-harder.gif", "https://c.tenor.com/np4F_PYhEwYAAAAM/cope-copeharder.gif"]
+
+cookie = ["https://c.tenor.com/lnT6BzzVsj4AAAAj/cookie-clicker-png.gif", "https://c.tenor.com/VG97Yfr9XLkAAAAj/tasty-chocolate.gif", "https://c.tenor.com/nx38WD8XJwEAAAAj/cookies-dekaron.gif", "https://c.tenor.com/TJREb7mszbwAAAAM/cat-cookies-cookies.gif", "https://c.tenor.com/Gyir-IHh1SsAAAAM/spongebob-cookies.gif"]
+
+trump = ["https://c.tenor.com/wU2a71ocaRgAAAAM/donald-trump-floss.gif", "https://c.tenor.com/Zi0kbpHwo8QAAAAM/trump-sad.gif", "https://c.tenor.com/6s_JcJ5qMzcAAAAM/president-trump-america.gif", "https://c.tenor.com/LPaOZv3nIiIAAAAM/trump-laugh-donald-trump.gif", "https://c.tenor.com/AJaswDYsKAQAAAAM/trump-2020.gif","https://i.imgur.com/SZwn4tj.jpeg", "https://i.imgur.com/UjESQZA.jpeg", "https://i.imgur.com/pgcYsEm.jpeg", "https://i.imgur.com/hmnwkUf.jpeg", "https://i.imgur.com/23KJcjW.jpeg","https://i.imgur.com/qjHKSTI.jpeg", "https://i.imgur.com/Ft6tyyB.jpeg", "https://i.imgur.com/H0Cy4KZ.jpeg", "https://i.imgur.com/D2cBuh1.jpeg"]
+
+
+EG = ["LINK", "LINK", "LINK", "LINK", "LINK"]
